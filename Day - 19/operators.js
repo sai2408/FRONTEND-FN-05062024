933b4b1a7e8dced343072cc372487e0db1718cab
Venkata sai vardhan Thimmisetty <57175225+sai2408@users.noreply.github.com>
2024-07-03 16:36:27 +0530

    Add files via upload

---
+++ Day - 19/operators.js
@@ -0,0 +1,135 @@
+console.log("Operatos in Js");
+//Arithemetic Operators
+/*
+let a = 2;
+let b = 3;
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(b/a);
+console.log(a**b);
+*/
+//Assignment Operators
+/*
+var a = 10;
+console.log(a);
+a = a + 2;
+console.log(a);
+a += 2;
+console.log(a);
+var a = 10;
+console.log(a);
+a = a - 2;
+console.log(a);
+a -= 2;
+console.log(a);
+var a = 5;
+console.log(a);
+a = a * 2;
+console.log(a);
+a *= 2;
+console.log(a);
+var a = 10;
+console.log(a);
+a = a / 2;
+console.log(a);
+a /= 2;
+console.log(a);
+var a = 10;
+console.log(a);
+a = a % 2;
+console.log(a);
+a %= 2;
+console.log(a);
+*/
+//Comparision Operators
+/*
+console.log("Comparision Operators");
+//Equals to
+/*
+var a = 10;
+var b = "10";
+console.log(a==b);
+*/
+//Strictly Equals to
+/*
+var a = 10;
+var b = "10";
+console.log(a===b);
+*/
+//Not Equals
+/*
+var a = 10;
+var b = "10";
+console.log(a!=b);
+*/
+//Strictly Not Equals
+/*
+var a = 10;
+var b = "10";
+console.log(a!==b);
+*/
+/*
+var a = 10;
+var b = 20;
+console.log(a>b);
+console.log(a<b);
+console.log(a>=b);
+console.log(a<=b);
+*/
+//Logical Operators
+/*
+console.log("Logical Operators");
+var age = 10;
+var voter = true;
+console.log(age>=18);
+console.log(voter==true);
+console.log((age>=18) && (voter==true))
+console.log((age>=18) || (voter==true))
+console.log(!(age>=18));
+*/
+//Unary Operator
+/*
+console.log("Unary Operator");
+var x = 5;
+console.log(+x);
+var y = 5;
+console.log(-y);
+*/
+//Ternary Operator
+/*
+console.log("Ternary Operator");
+var age = 28;
+var result = (age>=18) ? "Can Vote " : "Can not Vote ";
+console.log(result);
+*/
+//Increment and Decrement Operators
+/*
+console.log("Increment and Decrement Operators")
+var a1 = 100;
+console.log(a1++);
+var a2 = 100;
+console.log(++a2)
+var b1 = 200;
+console.log(b1--);
+console.log(b1);
+var b2 = 200;
+console.log(--b2);
+*/
+//String Concatenation Operator
+/*
+console.log("String Concatenation Operator");
+var a = "Hello";
+var b = "Codegnan";
+console.log(a+ " " +b);
+*/
+
+
+
+
+
+
+
+
+
+
---
+++ Day - 19/primitive.js
@@ -0,0 +1,121 @@
+console.log("Primitive Data Types")
+//Number
+/*
+var a = 10;
+let b = 20;
+const c = 30;
+console.log(a);
+console.log(b)
+console.log(c);
+*/
+//float
+/*
+var a = 10.45;
+let b = 34.56;
+const c = 35.78;
+console.log(a);
+console.log(b);
+console.log(c);
+*/
+//String
+/*
+var a = "Sai";
+let b = "10";
+const c = "45.67";
+console.log(a);
+console.log(b);
+console.log(c);
+*/
+//Boolean
+/*
+var a = true;
+let b = false;
+let c = true;
+console.log(a);
+console.log(b);
+console.log(c);
+*/
+//Undefined
+/*
+var a;
+let b;
+//const c;
+console.log(a);
+console.log(b);
+console.log(c);
+*/
+//Null
+/*
+let a = null;
+var b = null;
+const c = null;
+console.log(a);
+console.log(b);
+console.log(c);
+*/
+//Symbol
+/*
+let a = Symbol("sai");
+var b = Symbol(123);
+const c = Symbol(34.56);
+console.log(a);
+console.log(b);
+console.log(c);
+*/
+//Special Data types
+//NaN
+/*
+var a = NaN;
+var b = Number.NaN;
+let c = NaN;
+console.log(a);
+console.log(b);
+console.log(c);
+*/
+//BigInt
+/*
+var a = 12356;
+var b = 1234578902345678909876543;
+let c = 345678765434567876543456789;
+console.log(a);
+console.log(b);
+console.log(c);
+*/
+//Infinity
+/*
+let a = Infinity;
+var b = -Infinity;
+const c = Infinity;
+console.log(a);
+console.log(b);
+console.log(c);
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
