fef84cf8f1388b6d00a6e7ea13611572404cad37
Venkata sai vardhan Thimmisetty <57175225+sai2408@users.noreply.github.com>
2024-07-04 16:29:18 +0530

    Add files via upload

---
+++ Day - 20/abc.js
@@ -0,0 +1,94 @@
+//Conditional statements
+console.log("conditional Statements");
+//If Statement
+/*
+let age = 17;
+if(age>=18){
+    console.log("Yes you can Vote!");
+}
+console.log("Hello WOrld");
+*/
+//IF Else
+/*
+let age1 = 17;
+if(age1>=18){
+    console.log("Yes, You can Vote !");
+}
+else{
+    console.log("No, You can n ot vote");
+}
+console.log("Hello World2");
+*/
+//If Else If Else
+/*
+let age = 17;
+voter = false;
+if (age >= 18){
+    console.log("Yes, You can vote!")
+}
+else if (voter == true){
+    console.log("Yes, You can vote now!");
+}
+else{
+    console.log("You can not vote1");
+}
+*/
+/*
+let age = 17;
+voter = true;
+if (age>=18 || voter==true){
+    console.log("Yes  you can");
+}
+else{
+    console.log("You can not");
+}
+*/
+//Switch case
+/*
+let count = 5;
+switch(count){
+    case 1:
+        console.log("You gained 10% discount");
+        break;
+    case 2:
+        console.log("You gained 20% discount");
+        break;
+    case 3:
+        console.log("You gained 30% discount");
+        break;
+    case 4:
+        console.log("You gained 40% discount");
+        break;
+    default:
+        console.log("Select product first");
+}
+*/
+//Nested Conditional Statements
+/*
+var age= 18;
+var voter = true;
+if(age>=18){
+    if(voter==true){
+        console.log("You can VOte");
+    }
+}
+else{
+    console.log("You can not vote");
+}
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Day - 20/looping.js
@@ -0,0 +1,54 @@
+//For Loop
+//For Loop
+/*
+for (var i=0;i<=5;i++){
+    console.log(i);
+}
+for (var i=5;i>=0;i--){
+    console.log(i);
+}
+for (var i=0;i<=5;i--){
+    console.log(i);
+}*/
+//For In
+/*
+var contact = {
+    name : "Sai",
+    mobile : 7893570611,
+    email : "saivardhan@codegnan.com"
+}
+for (var x in contact){
+    console.log(x + " : " +contact[x]);
+}
+*/
+//for of
+/*
+var lst = ["a","b","c","d"];
+for (var x of lst){
+    console.log(x);
+}
+*/
+/*
+var lst = [10,11,12,34,57,81,4];
+for (var x of lst){
+    if (x%2==0){
+        console.log(x);
+    }
+}
+*/
+//while Loop
+//While Loop
+/*
+var x = 5;
+while(x>=1){
+    console.log(x);
+    x = x - 1;
+}
+*/
+/*
+var x = 5;
+do {
+    console.log(x);
+    x = x - 1
+} while(x>=1);
+ */
