dddbaf7d70258414d72c96b292cd7cb7e165c618
Venkata sai vardhan Thimmisetty <57175225+sai2408@users.noreply.github.com>
2024-07-06 12:43:34 +0530

    Add files via upload

---
+++ Day - 22 (Test Execution - 3)/sample.js
@@ -0,0 +1,75 @@
+/*
+var x = 0;
+if (x >= 0){
+    res = 0;
+    while (x != 0){
+        var r = x%10;
+        res = (res*10)+r;
+        x = parseInt(x/10);
+    }
+    console.log(res);
+}
+else{
+    x = x * -1;
+    res = 0;
+    while (x != 0){
+        var r = x%10;
+        res = (res*10)+r;
+        x = parseInt(x/10);
+    }
+    console.log(-1*res);
+
+}
+*/
+/*
+var a = 10;
+var b = 20;
+var c = 30;
+if (a>b && a>c){
+    a = 0
+}
+else if (b>a && b>c){
+    b = 0
+}
+else{
+    c = 0
+}
+if (a>b && a>c){
+    console.log(a);
+}
+else if (b>a && b>c){
+    console.log(b);
+}
+else{
+    console.log(c);
+}
+*/
+/*
+var x = 100;
+var y = 20;
+var c = y/2;
+var d = (x-c);
+console.log(d);
+*/
+/*
+var a = 10;
+var b = 20;
+var a = a + b;
+var b = a - b;
+var a = a - b;
+console.log(a);
+console.log(b);
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
