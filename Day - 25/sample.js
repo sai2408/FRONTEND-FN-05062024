1b4142965daff3e3b4ced2eb1665a566ca60bd93
Venkata sai vardhan Thimmisetty <57175225+sai2408@users.noreply.github.com>
2024-07-10 21:42:42 +0530

    Add files via upload

---
+++ Day - 25/sample.js
@@ -0,0 +1,21 @@
+console.log("Strings");
+//Creation of Strings
+var x = "Sai Vardhan";
+console.log(x);
+//Finding length
+var a = x.length;
+console.log(a);
+//Access Elements
+console.log(x[4]);
+//Concatination
+var v1 = "Sai";
+var v2 = "Vardhan";
+var v3 = v1 + " " + v2 + " !";
+console.log(v3);
+console.log(v1)
+
+x1 = "Sai";
+x2 = "Vardhan";
+var x3 = x1.concat(" ",x2," !");
+console.log(x3);
+console.log(x1);
