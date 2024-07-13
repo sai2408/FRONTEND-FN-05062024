27134965be6fe9cedc4b85ac738c6458faf643eb
Venkata sai vardhan Thimmisetty <57175225+sai2408@users.noreply.github.com>
2024-07-13 12:52:48 +0530

    Add files via upload

---
+++ Day - 28/sample.js
@@ -0,0 +1,38 @@
+//Fctors of a number in arr
+/*
+array = [10,20,7,4,3,5,30]
+n = 30
+for (var x of array){
+    if ((n%x)==0){
+        console.log(x);
+    }
+}*/
+//Perfect numbers in array
+/*
+array = [3,6,2,28]
+for (var x of array){
+    summ = 0
+    for (var i=0;i<x;i++){
+        if (x%i==0){
+            summ = summ + i;
+        }
+    }
+    if (summ==x){
+        console.log(x);
+    }
+}
+*/
+//Factorial values of even intergers
+//in an array
+/*
+arr = [109,2,4,7,6,21]
+for (var x of arr){
+    if (x%2 == 0){
+        summ = 1
+        for (var i=1;i<=x;i++){
+            summ = summ * i;
+        }
+        console.log(summ);
+    }
+}
+*/
