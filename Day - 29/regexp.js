483eb1b36ac4aef29e0a475dcd985aa0f8714b94
Venkata sai vardhan Thimmisetty <57175225+sai2408@users.noreply.github.com>
2024-07-15 18:14:22 +0530

    Add files via upload

---
+++ Day - 29/regexp.js
@@ -0,0 +1,51 @@
+console.log("Reg Exp");
+
+// \d
+/*
+var pattern = /\d/g;
+var text = "My age is 28 .";
+var res = text.match(pattern);
+console.log(res);
+*/
+//\D
+/*
+var pattern = /\D/g;
+var text = "My age is 28 .";
+var res = text.match(pattern);
+console.log(res);
+*/
+//\s
+/*
+var pattern = /\s/g;
+var text = "My age is 28 .";
+var res = text.match(pattern);
+console.log(res);
+*/
+//\S
+/*
+var pattern = /\S/g;
+var text = "My age is 28 .";
+var res = text.match(pattern);
+console.log(res);
+*/
+//\w
+/*
+var pattern = /\w/g;
+var text = "My age is 28_ .";
+var res = text.match(pattern);
+console.log(res);
+*/
+//\W
+/*
+var pattern = /\W/g;
+var text = "My age is 28_ .";
+var res = text.match(pattern);
+console.log(res);
+*/
+//\b
+/*
+var pattern = /\bage\b/g;
+var text = "The age is 20 age .";
+var res = text.match(pattern);
+console.log(res);
+*/
