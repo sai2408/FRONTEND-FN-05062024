a6614cce0609b9411ea1b7ee24349f7304187fc7
Venkata sai vardhan Thimmisetty <57175225+sai2408@users.noreply.github.com>
2024-07-22 19:13:27 +0530

    Add files via upload

---
+++ Day - 34/sample.js
@@ -0,0 +1,9 @@
+var x = prompt("Enter Your name : ");
+var z = confirm("Are you 18+ ?");
+if (z == true){
+    console.log("Welcome " + x + " !");
+}
+else{
+    console.log("Try agin when you cross 18");
+}
+
